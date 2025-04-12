a929f0fbfa7d5e1a149e0ab902d391e9cace6acc
kingpriyanshu21 <priyanshusingh8233@gmail.com>
2025-04-12 14:48:37 +0530

    push code

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,13 @@ const PORT=process.env.PORT || 3000
 
 const app=express()
 
-app.use(cors())
+app.use(cors(
+    {
+        origin: 'https://readify-two.vercel.app/',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        credentials: true,
+    }
+))
 app.use(express.json())
 
 // Database connection
